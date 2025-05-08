4094ddf8cb0983931a45dc82bcae6b5a24527435
hetlodariya22 <het.lodariya@weingenious.in>
2025-05-08 16:41:31 +0530

    Revert "ssss"
    This reverts commit 586b59883031d31098dff187bf2ac5ccd8be4d96.

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,7 +6,6 @@ import {cn} from "@/lib/utils";
 import {useRouter} from "next/navigation";
 import {vapi} from "@/lib/vapi.sdk";
 import {interviewer} from "@/constants";
-import {createFeedback} from "@/lib/actions/general.action";
 
 enum CallStatus {
     INACTIVE = "INACTIVE",
@@ -65,11 +64,10 @@ const Agent = ({ userName, userId, type, interviewId, questions } : AgentProps) 
     const handleGenerateFeedback = async (messages : SavedMessage[]) => {
         console.log('Generate feedback here');
 
-        const { success, feedbackId : id } = await createFeedback({
-            interviewId : interviewId!,
-            userId : userId!,
-            transcript : messages
-        })
+        const { success, id } = {
+            success : true,
+            id : 'feedback-id',
+        }
 
         if(success && id){
             router.push(`/interview/${interviewId}/feedback`);
