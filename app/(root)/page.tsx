f10a42c5f000bbcb5ba654f63cbcb033840d7ab9
hetlodariya22 <het.lodariya@weingenious.in>
2025-05-08 15:16:38 +0530

    interview feedback

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import {dummyInterviews} from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
-import {getCurrentUser, getInterviewsByUserId, getLatestInterviews} from "@/lib/actions/auth.action";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import { getInterviewsByUserId, getLatestInterviews } from "@/lib/actions/general.action";
 
 const Page = async () => {
     const user = await getCurrentUser();
@@ -14,8 +14,8 @@ const Page = async () => {
         await getLatestInterviews({ userId : user?.id! })
     ]);
 
-    const hasPastInterviews = userInterviews?.length > 0;
-    const hasUpcomingInterviews = latestInterviews?.length > 0;
+    const hasPastInterviews = userInterviews?.length! > 0;
+    const hasUpcomingInterviews = latestInterviews?.length! > 0;
 
     return (
         <>
